938205a54b04daab4f45c39fac3025d375268bba
zdenek.hrazdira <zdenek.hrazdira@thermofisher.com>
2022-05-03 15:50:39 +0200

    setups

--- script/setup_fftw.sh
+++ script/setup_fftw.sh
@@ -5,10 +5,12 @@ sudo chmod 755 fftw-3.3.10.tar.gz
 tar -xf fftw-3.3.10.tar.gz && mv fftw-3.3.10 fftw && cd fftw
 mkdir build && cd build
 
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release
+# double
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DENABLE_AVX=ON -DENABLE_AVX2=ON -DENABLE_SSE=ON -DENABLE_SSE2=ON 
 cmake --build . --config Release -j 6
 sudo ninja install
 
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DENABLE_FLOAT=ON
+# float
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DENABLE_FLOAT=ON -DENABLE_AVX=ON -DENABLE_AVX2=ON -DENABLE_SSE=ON -DENABLE_SSE2=ON 
 cmake --build . --config Release -j 6
 sudo ninja install
--- script/setup_pffft.sh
+++ script/setup_pffft.sh
@@ -4,6 +4,6 @@ git clone --recursive https://github.com/marton78/pffft.git
 cd pffft
 mkdir build && cd build
 
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DUSE_SIMD=ON
 cmake --build . --config Release -j 6
 sudo ninja install
