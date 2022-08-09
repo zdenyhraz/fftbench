b67c198738336b329c4df30024b8cf3a4cf9eaf1
zdenek.hrazdira <zdenek.hrazdira@thermofisher.com>
2022-08-09 17:34:05 +0200

    pix

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # FFT benchmarks
-
-- [OpenCV-IPP](https://www.intel.com/content/www/us/en/developer/articles/technical/enabling-ipp-on-opencv-windows-and-ubuntu.html)
 - [FFTW](https://www.fftw.org/) with [FFTW_ESTIMATE](https://www.fftw.org/fftw3_doc/Planner-Flags.html)
 - [FFTW](https://www.fftw.org/) with [FFTW_MEASURE](https://www.fftw.org/fftw3_doc/Planner-Flags.html)
+- [FFTW](https://www.fftw.org/) with [FFTW_PATIENT](https://www.fftw.org/fftw3_doc/Planner-Flags.html)
+- [OpenCV-IPP](https://www.intel.com/content/www/us/en/developer/articles/technical/enabling-ipp-on-opencv-windows-and-ubuntu.html)
 - [PocketFFT](https://gitlab.mpcdf.mpg.de/mtr/pocketfft/tree/cpp)
 - [PFFFT](https://github.com/marton78/pffft)
+- [KFR](https://www.kfrlib.com/newdocs/)
 
+
+<p align="center">
+  <img src="data/fftbench_example.png" width="70%">
+</p>
--- tools/show_benchmark_results.py
+++ tools/show_benchmark_results.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 
 
 class BenchmarkEntry:
-  def __init__(self, size, time):
-    self.sizes = np.array([size])
-    self.times = np.array([time])
+    def __init__(self, size, time):
+        self.sizes = np.array([size])
+        self.times = np.array([time])
 
 
 def find_nth(haystack, needle, n):
-  start = haystack.find(needle)
-  while start >= 0 and n > 1:
-    start = haystack.find(needle, start+len(needle))
-    n -= 1
-  return start
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
 
 
 time_unit_input = 1e-6
@@ -23,35 +23,35 @@ entries = {}
 minsize = np.inf
 maxsize = 0
 
-with open('build/fftbench.csv', newline='') as csvfile:
-  reader = csv.reader(csvfile, delimiter=',')
-  for row in reader:
-    string = ', '.join(row)
-    if " | " in string:
-      print("Processing", string)
-      size = int(string[0:string.find(" | ")].strip())
-      name = string[string.find(" | ")+2:string.find(",")].strip()
-      time = float(string[find_nth(string, ",", 2)+1:find_nth(string, ",", 3)].strip())/(time_unit/1e-6)  # [ms]
-      minsize = min(minsize, size)
-      maxsize = max(maxsize, size)
+with open('data/fftbench_example.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        string = ', '.join(row)
+        if " | " in string:
+            print("Processing", string)
+            size = int(string[0:string.find(" | ")].strip())
+            name = string[string.find(" | ")+2:string.find(",")].strip()
+            time = float(string[find_nth(string, ",", 2)+1:find_nth(string, ",", 3)].strip())/(time_unit/1e-6)  # [ms]
+            minsize = min(minsize, size)
+            maxsize = max(maxsize, size)
 
-      if name in entries:
-        entries[name].sizes = np.append(entries[name].sizes, size)
-        entries[name].times = np.append(entries[name].times, time)
-      else:
-        entries[name] = BenchmarkEntry(size, time)
+            if name in entries:
+                entries[name].sizes = np.append(entries[name].sizes, size)
+                entries[name].times = np.append(entries[name].times, time)
+            else:
+                entries[name] = BenchmarkEntry(size, time)
 
 
 xticksvalues = []
 xtickslabels = []
 for exp in range(int(np.log2(minsize)), int(np.log2(maxsize))+1):
-  xticksvalues.append(np.power(2, exp))
-  xtickslabels.append("$2^{" + "{}".format(int(exp)) + "}$")
+    xticksvalues.append(np.power(2, exp))
+    xtickslabels.append("$2^{" + "{}".format(int(exp)) + "}$")
 
 plt.figure(figsize=(10, 10))
 plt.subplot(2, 1, 1)
 for name, entry in entries.items():
-  plt.plot(entry.sizes, entry.times, "*-", label=name, linewidth=2)
+    plt.plot(entry.sizes, entry.times, "*-", label=name, linewidth=2)
 plt.xscale("log")
 plt.xlabel("FFT size")
 plt.ylabel("time [ms]")
@@ -60,7 +60,7 @@ plt.legend()
 
 plt.subplot(2, 1, 2)
 for name, entry in entries.items():
-  plt.plot(entry.sizes, entry.times, "*-", label=name, linewidth=2)
+    plt.plot(entry.sizes, entry.times, "*-", label=name, linewidth=2)
 plt.xscale("log")
 plt.yscale("log")
 plt.xlabel("FFT size")
@@ -69,4 +69,5 @@ plt.xticks(xticksvalues, xtickslabels)
 plt.legend()
 
 plt.tight_layout()
+plt.savefig("data/fftbench_example.png")
 plt.show()
