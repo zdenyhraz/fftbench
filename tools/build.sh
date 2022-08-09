2f12dc5c85e73731a34f60e65a47d9334865d7a4
zdenek.hrazdira <zdenek.hrazdira@thermofisher.com>
2022-08-09 17:48:15 +0200

    clang

--- tools/build.sh
+++ tools/build.sh
@@ -2,6 +2,5 @@
 
 rm -rf build
 mkdir build && cd build
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 cmake --build . --config Release
-
