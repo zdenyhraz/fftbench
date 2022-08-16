597567bf82607c5e9c35a0ab0795eff30b9a50dd
zdenek.hrazdira <zdenek.hrazdira@thermofisher.com>
2022-08-16 16:25:41 +0200

    +/+

--- tools/download_results.sh
+++ tools/download_results.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 scp root@10.170.111.80:/home/root/fftbench/data/fftbench.csv data  # atom
+python3 tools/show_benchmark_results.py
--- tools/setup_pffft.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone --recursive https://github.com/marton78/pffft.git
-cd pffft
-mkdir build && cd build
-
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DUSE_SIMD=ON
-cmake --build . --config Release -j 6
-sudo ninja install
